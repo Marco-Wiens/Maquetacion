960bb0c2428d8d14d62f29b01e22da3ce060ab42
Marco-Wiens <marco.wiens@gmail.com>
2023-10-30 16:36:38 +0100

    se añade el js

---
+++ AgenciaDeViajes/viajes.js
@@ -0,0 +1,25 @@
+
+let arrObj = [];
+function guardarObjeto(){
+    
+    let obj = {};
+    obj.nombre = document.getElementById("name").value;
+    obj.origen = document.getElementById("origen").value;
+    obj.destino = document.getElementById("destino").value;
+    obj.NPersonas = document.getElementById("npers").value;
+    obj.fecha = document.getElementById("date").value;
+    arrObj.push(obj);
+
+    console.log(arrObj);
+    
+
+}
+
+function mostrar(){
+
+    for(let obj of arrObj){
+        if(obj.destino.toUpperCase() == "MALLORCA" || obj.destino.toUpperCase() == "CANARIAS" || obj.destino.toUpperCase() == "GALICIA"){
+            console.log(obj);
+        }
+    }
+}
